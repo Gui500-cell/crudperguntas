97e4812a542255d35eb3b309f2dad6452dc1a7d8
GuilhermeBarreto <gb9756092@gmail.com>
2025-12-02 20:58:40 -0300

    Primeiro commit do projeto CRUD Perguntas

---
+++ Logica.py
@@ -0,0 +1,37 @@
+import json, os
+import tkinter as tk
+from tkinter import messagebox
+
+arquivo = "perguntas.json"
+
+# Função para json
+def carregar_dados():
+    if os.path.exists(arquivo):
+        with open (arquivo, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+def salvar_dados(perguntas):
+    with open(arquivo, "w", encoding="utf-8") as f:
+        json.dump(perguntas, f, indent=4, ensure_ascii=False)
+
+# Função CRUD        
+def adicionar(perguntas, nova):
+    try:
+      perguntas.append(nova)
+      salvar_dados(perguntas)
+    except ValueError:
+        messagebox.showerror("É necessario preencher o campo vazio.")
+
+def atualizar(perguntas, indice, nova):
+    try:
+     perguntas[indice] = nova
+     salvar_dados(perguntas)
+    except ValueError:
+        messagebox.showerror("É necessario preencher o campo vazio.")
+def excluir(perguntas, indice):
+    perguntas.pop(indice)
+    salvar_dados(perguntas)                             
+
+#def limpar():
+# pergunta.delete(0, tk.END)
+        
